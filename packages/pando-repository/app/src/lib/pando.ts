7c15fdab2c23405537f2a9975f3adebf5597d79c
cd10012 <ced361@nyu.edu>
2019-04-13 16:42:58 +0200

    create typescript object for ipld-git

--- packages/pando-repository/app/src/lib/pando.ts
+++ packages/pando-repository/app/src/lib/pando.ts
@@ -1,3 +1,11 @@
+import IPLD from 'ipld'
+import IPLDGit from 'ipld-git'
+const ipfs = IPFS({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
+const ipld = new IPLD({
+  blockService: ipfs.block,
+  formats: [IPLDGit],
+})
+
 export interface IPLDAuthor {
   name: string
   email: string
@@ -29,8 +37,37 @@ export interface Commit extends IPLDCommit { //What if instead of using a new in
   sha: string
 }
 
-//function generateIPLDCommit(obj: any): IPLDCommit {}
-//function getModifiedTree(root: Tree): ModifiedTree {}
-//function convertCommitToIPLDCommit(commit: Commit): IPLDCommit {}
+export function generateIPLDCommit(commit: any): IPLDCommit {
+  const authorType = new IPLDAuthor({
+    date: commit.author.date,
+    name: commit.author.name,
+    email: commit.author.email
+  });
+
+  const committerType = new IPLDAuthor({
+    date: commit.committer.date,
+    name: commit.committer.name,
+    email: commit.committer.email
+  });
+
+  let parents = [];
+
+  for (let i in commit.parents) {
+    parents.push( commit.parents[i] as Tree );
+  }
+
+  const tree = commit.tree as Tree;
+
+  return new IPLDCommit({
+    author: authorType,
+    commiter: committerType,
+    message: commit.message,
+    parents: parents,
+    tree: tree
+  })
+}
+
+//export function fetchModifiedTree(root: Tree): ModifiedTree {}
+//export function convertCommitToIPLDCommit(commit: Commit): IPLDCommit {}
 
 
